97a2cbdbafc2c7229de613730af53bf5bb9c017c
Perry Mason <perry.mason@live.co.uk>
2017-10-16 12:42:32 +0100

    deploy

--- psake.ps1
+++ psake.ps1
@@ -60,7 +60,7 @@ Task Build -Depends Test {
     $lines
 
     # Bump the script version
-    Update-ScriptFileInfo -Path $ProjectRoot\Get-MacVendor\Get-MacVendor.ps1 -Version $env:APPVEYOR_BUILD_VERSION -Author "PM091" -Description "Get-MacVendor"  -Force 
+    New-ScriptFileInfo -Path $ProjectRoot\Get-MacVendor\Get-MacVendor.ps1 -Version $env:APPVEYOR_BUILD_VERSION -Author "PM091" -Description "Get-MacVendor"  -Force 
 }
 
 Task Deploy -Depends Build {
