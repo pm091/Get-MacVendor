4713962cfd27ac877910b6851ee4de5a662c7762
pm091 <pm_091@outlook.com>
2019-05-08 00:55:21 +0100

    Remove Broke Stuff Deploy!

--- Tests/Get-MacVendor.Tests.ps1
+++ Tests/Get-MacVendor.Tests.ps1
@@ -1,25 +1,6 @@
 $PSVersion = $PSVersionTable.PSVersion.Major
 . $env:BHProjectPath\Get-MacVendor\Get-MacVendor.ps1
 
-
-
-#Integration test example
-Describe "Get-Macvendor PS$PSVersion Integrations tests" {
-
-    Context 'Strict mode' { 
-
-        Set-StrictMode -Version latest
-
-        It 'should get valid data' {
-            $Output = Get-MacVendor -MacAddress 00:00:0C:00:00:00
-            $Output.GetType().name | Should be 'PSCustomObject'
-            $Output.MacAddress -match "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$" | Should be $true
-            $Output.Vendor -contains 'Cisco Systems, Inc'
-        }
-    }
-}
-
-
 #Unit test example
 Describe "Get-MacVendor PS$PSVersion Unit tests" {
 
